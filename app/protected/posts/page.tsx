1d882c8557c8d56818331e6c40bebcaba93e87ee
enzzzooo <ed123658@gmail.com>
2024-03-10 19:32:09 -0500

    making a grid layout

--- app/protected/posts/page.tsx
+++ app/protected/posts/page.tsx
@@ -17,13 +17,13 @@ export default async function page() {
   
   }
   return(
-    <>
-    {posts && posts.map((post) => 
-    <div key={post.id}>
-      <h1 className="text-2xl font-bold mb-2">{post.title}</h1>
-      <p>{post.text} </p>
-    </div>)}
-    </>
+    <div className="grid grid-rows-4 grid-flow-col gap-4">
+      {posts && posts.map((post) => 
+      <div key={post.id}>
+        <h1 className="text-2xl font-medium mb-2">{post.title}</h1>
+        <p>{post.text} </p>
+      </div>)}
+    </div>
   )
 }
   
